56531da4521f564695eec0efc89b29b1232f87c8
unknown <omarhj1992@gmail.com>
2020-06-08 23:32:22 +0300

    nextfix  003

--- src/Blog/posts.js
+++ src/Blog/posts.js
@@ -114,7 +114,7 @@ class Posts extends Component {
   componentDidMount() {
     this.handleTransition();
     axios
-      .get("/api/get/allposts")
+      .get("https://my-social-blog-api-server.herokuapp.com/api/get/allposts")
       .then(res => {
         console.log(res.data);
         this.props.set_posts(res.data);
